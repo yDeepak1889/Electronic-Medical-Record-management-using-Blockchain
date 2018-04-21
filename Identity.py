b2719c1cc573953d12a897bfb097372a29aa5096
stark03 <tusharmurarka@gmail.com>
2018-04-22 03:21:33 +0530

    Identity generation is possible, generates pubKey, QR

---
+++ Identity.py
@@ -0,0 +1,28 @@
+import scrypt
+import pyqrcode
+
+
+#Code to generate PNG out of QR code object
+#big_code.png('code.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
+
+
+
+class Identity(object):
+	
+	def generateKey(self, password):
+
+		randomSalt = "HelloWorld"
+		pubKey = scrypt.hash(password, randomSalt)
+		QR = pyqrcode.create(str(pubKey), error='L', version=8, mode='binary')
+		
+		return pubKey, QR
+
+
+
+
+
+i = Identity()
+pubKey, QR = i.generateKey("Hello there")
+print(pubKey)
+
+#QR.png('QRCode1.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
